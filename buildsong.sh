a7237390a1bb15f9413606ced29a784f11254eeb
Evan Casey <evan.casey92@gmail.com>
2014-06-28 17:10:26 -0400

    added drake template

--- buildsong.sh
+++ buildsong.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 # create commits audio file
-cat $1 | say -o temp.aiff
+cat $1 | say -o tmp/commits.aiff
 
-# merge with secondary audio file
-ffmpeg -i temp.aiff -i $2 -filter_complex amerge -c:a libmp3lame -q:a 4 output.mp3
+TIM="goodbye"
+
+if [ $3 == "drake" ] ;then
+
+  TIM="hello"
+  echo "$TIM"
+  # merge with secondary audio file
+  #ffmpeg -i tmp/commits.aiff -i $2 -filter_complex amerge -c:a libmp3lame -q:a 4 output.mp3
+
+fi
+
 
 # upload to 
 
