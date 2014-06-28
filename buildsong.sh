2d4e491e00d4f59cfc46affde4a0197cc3d1e70c
Evan Casey <evan.casey92@gmail.com>
2014-06-28 19:21:24 -0400

    changed file name

--- buildsong.sh
+++ buildsong.sh
@@ -2,7 +2,7 @@
 # ./buildsong.sh tmp/drake-template.txt output.mp3 drake
 
 # TODO: change later
-COMMITS_AUDIO_PATH=${1:0:14}.aiff
+COMMITS_AUDIO_PATH=${1:0:14}.mp3
 
 cat $1 | say -o $COMMITS_AUDIO_PATH
 
@@ -20,7 +20,7 @@ FILE_ID=$(echo $RESPONSE| grep 'file_id' | cut -d '=' -f 5 | cut -d ' ' -f 1)
 if [ $3 == "drake" ] ;then
 
   # autotune with drake midi  
-  curl 'https://api.sonicapi.com/process/elastiqueTune?input_file=@/tmp/drake-temp.aiff&access_id=6cf9eac8-de22-4190-8813-ee13df780350&format=json'
+  curl 'https://api.sonicapi.com/process/elastiqueTune?input_file=https://github.com/evancasey/git-music/blob/master/tmp/drake-temp.aiff&access_id=6cf9eac8-de22-4190-8813-ee13df780350&format=json'
 
   echo $RESPONSE
  
